faf6f671c45de1894abc0e1bec35f3b8ec045b21
ayush286 <89552326+ayush286@users.noreply.github.com>
2021-09-01 17:03:39 +0530

    Update searchInSortedMatrix.py

--- searchInSortedMatrix.py
+++ searchInSortedMatrix.py
@@ -10,3 +10,27 @@ def searchInSortedMatrix(matrix, target):
 		else:
 			node[1] -= 1
 	return [-1, -1]
+
+###Solution 2
+#O(rlogc) Time | O(1) Space where r is number of rows of matrix and 
+#c is number of columns of matrix 
+def searchInSortedMatrix(matrix, target):
+    # Write your code here.
+	col = 0
+	for row in range(len(matrix)):
+		low = 0
+		high = len(matrix[row]) - 1
+		if target < matrix[row][low]:
+			return [-1, -1]
+		if target > matrix[row][high]:
+			continue
+		while low <= high:
+			middle = (low + high) // 2
+			if target == matrix[row][middle]:
+				return [row, middle]
+			elif target < matrix[row][middle]:
+				high = middle - 1
+			else:
+				low = middle + 1
+	return [-1, -1]
+
